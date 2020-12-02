f7136a7ecbafb04806b9623f0f8177c86a37650c
erkearney <erickearney@protonmail.com>
2020-12-02 16:52:33 -0700

    rename and cleanup

--- README.md
+++ README.md
@@ -1,14 +1,2 @@
-# Ubuntu_setup
-## One day I will turn this all into a script
-Since I distro hop/reinstall often this will be a collection of the annoying things I need to remember to do to get Ubuntu to actually work
-
-
-run sudo update-initramfs -u to update initramfs
-run sudo cat /sys/module/nvidia_drm/parameters/modeset to confirm
-
-
-sudo apt install gnome-tweaks		SET TO DARK
-sudo apt install vim
-sudo apt install git
-git config --global user.email
-git config --global user.name
+# Debian setup
+Since I distro hop/reinstall often this will be a collection of the annoying things I need to remember to do to get Debian based distributions to actually work
---
+++ debian_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+apt update
+apt upgrade
+echo "Fixing nvidia-card screen tearing issues"
+echo "options nvidia-drm modeset=1" > /etc/modprobe.d/nvidia-drm-nomodeset.conf
+update initramfs -u
+echo "Veryify this outputs Y"
+cat /sys/module/nvidia_drm/parameters/modeset
+echo "Installing vim and downloading my .vimrc"
+apt install vim
+wget -O /home/$(logname)/.vimrc https://raw.githubusercontent.com/erkearney/my-vimrc/master/.vimrc
+echo "Installing gnome tweaks, remember to set theme to dark"
+apt install gnome-tweaks
+echo "Installing java 11"
+apt install openjdk-11-jdk
+read -r -p "Do you want to download Minecraft> [y/N] " minecraft
+if [[ "$minecraft" == "y" ]]
+then
+wget https://www.minecraft.net/en-us/download/Minecraft.deb
+#apt install ./Minecraft.deb
+fi
+echo "Installing git, remember to run git config --global user.email/user.name"
+apt install git
